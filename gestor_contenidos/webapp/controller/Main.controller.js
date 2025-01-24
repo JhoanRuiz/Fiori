22458ecc50e302244ddcd5bd9acbe0d46e15a121
Jhoan Ruiz <jhoan.ruiz@co.ey.com>
2025-01-24 21:46:59 +0000

    commit gestor contenido v1

--- gestor_contenidos/webapp/controller/Main.controller.js
+++ gestor_contenidos/webapp/controller/Main.controller.js
@@ -1,10 +1,15 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent"
+], (Controller, UIComponent) => {
     "use strict";
 
     return Controller.extend("gestorcontenidos.controller.Main", {
         onInit() {
+        },
+        onBono: function(){
+            let oRouter = UIComponent.getRouterFor(this);
+            oRouter.navTo("RouteGestorContenido");
         }
     });
 });
