44ec39693efde9cb782b5be316df694e55db62a3
Jhoan Ruiz <jhoan.ruiz@co.ey.com>
2025-01-24 17:29:05 +0000

    Commit Gestor de Bono

---
+++ gestor_contenidos/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("gestorcontenidos.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ gestor_contenidos/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["gestorcontenidos/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
